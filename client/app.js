099a124e4d2af818834a2f0ee343b9d1a97ab75a
shicaih <shicaih@andrew.cmu.edu>
2022-04-04 13:38:53 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
 
 http.createServer(function (req, res) {
     fs.readFile(__dirname + req.url, function (err,data) {
+        console.log(req.url);
         if (err) {
             res.writeHead(404);
             res.end(JSON.stringify(err));
