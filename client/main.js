8255b40bc286dddc57743d94561a867082872797
shicaih <shicaih@andrew.cmu.edu>
2022-04-25 16:08:37 -0400

    Update main.js

--- client/main.js
+++ client/main.js
@@ -2358,7 +2358,7 @@ class Lobby extends Phaser.Scene {
 }
 
 let gameconfig = {
-  type: isMobile? Phaser.CANVAS : Phaser.WEBGL,
+  type: isMobile? Phaser.CANVAS : Phaser.AUTO,
   fps: { target: 40, forceSetTimeOut: true },
   backgroundColor: 0x222222,
   width: gameOptions.viewportWidth,
